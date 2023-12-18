c3f75cb069561a10a72ac3bd5c987e8f2cc32bbe
aakash vishwakarma <ashv9730@gmail.com>
2023-12-18 15:49:35 +0530

    hello aa

--- routes/index.js
+++ routes/index.js
@@ -5,29 +5,6 @@ import Product from "../models/UserModels";
 const userRouter = Router()
 
 userRouter.post('/newproduct', async (req,res) => {
-    // const {name, age} = req.body
-    // const user = new User({
-    //     name: name,
-    //     age: age
-    // })
-
-    // try {
-    //     const userSave = await user.save();
-    //     if (!userSave) {
-    //         res.status(201).json({
-    //             message: "user not svaed"
-    //         })
-    //     }
-    //     res.json({
-    //         message: "message from newuser",
-    //         userSave
-    //     })
-    //   } catch (error) {
-    //     res.status(500).send(error);
-    //   }
-    
-
-
     const user = new Product(req.body);
   
     try {
